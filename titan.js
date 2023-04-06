0bf277f1f3a12aa5ca0d5d035b4dd311545c9a18
mathis laurent <mathis.laurent@epitech.eu>
2023-04-06 21:52:46 +0200

    features'

--- titan.js
+++ titan.js
@@ -14,7 +14,7 @@ const fs = require('fs');
     }
     console.log("professions : ", professions)
     const file = fs.createWriteStream('log.csv');
-    file.write("Index;Prénom;Nom;Numéro;Intitulé\n");
+    file.write("Intitulé;Prénom;Nom;Numéro\n");
     let page = await browser.newPage();
     const keyboard = page.keyboard;
     await page.setViewport({ width: 1903, height: 969 });
@@ -49,24 +49,57 @@ const fs = require('fs');
         ]);
         console.log('\033[1;37m', '$> ', '\033[1;36m', "Recherche . . .");
         await wait(1000);
-        const currentlist = await page.$$('.item-professionnel-inner');
-        for (const currentProfile of currentlist) {
-            const strongElementHandle = await currentProfile.$('h2 a strong');
-            const firstNameElementHandle = await currentProfile.$('h2 a');
-            const firstNameElement= await page.evaluate(el => el.innerHTML, firstNameElementHandle);
-            // console.log(firstNameElementHandle)
+        let once = false;
+        while (1) {
+            const currentlist = await page.$$('.item-professionnel-inner');
+            for (const currentProfile of currentlist) {
+                const strongElementHandle = await currentProfile.$('h2 a strong');
+                const firstNameElementHandle = await currentProfile.$('h2 a');
+                const firstNameElement = await page.evaluate(el => el.innerHTML, firstNameElementHandle);
+                const phoneNumberElement = await currentProfile.$('.elements .tel');
+                try {
+                    const phoneNumberText = await page.evaluate(phoneNumberElement => phoneNumberElement.textContent, phoneNumberElement);
+                    // console.log(firstNameElementHandle)
 
-            let firstNameText = firstNameElement;
-            if (firstNameElement) {
-                // console.log(firstNameElement)
-            } else {
-                console.log('No first name found');
+                    let firstNameText = firstNameElement;
+                    let firstname =  firstNameText.substring(firstNameText.indexOf("</strong>") + "</strong>".length);
+                    if (firstNameElement) {
+                        // console.log(firstNameElement)
+                    } else {
+                        console.log('No first name found');
+                    }
+                    // const firstNameText = await page.evaluate(firstNameElement => firstNameElement.textContent, firstNameElementHandle);
+
+                    const strongText = await page.evaluate(strongElement => strongElement.textContent, strongElementHandle);
+
+                            phoneNumberText[0] == '0' && (phoneNumberText[1] == '6' || phoneNumberText[1] == '7') ?
+                            console.log('\033[1;32m', 'profil : ', strongText, ' ', firstname, ' - ',  phoneNumberText): 
+                            console.log('\033[1;32m', 'profil : ', strongText, ' ', firstname, ' - ', '\033[1;31m', 'Not a personnal phone number : ', phoneNumberText, '\033[0m');
+
+                    if (phoneNumberText[0] == '0' && (phoneNumberText[1] == '6' || phoneNumberText[1] == '7'))
+                        file.write(element + ';' + firstname + ';' + strongText + ';' + phoneNumberText + '\n')
+                } catch (err) {
+                    console.log('\033[1;31m', 'Missing field', '\033[0m')
+                }
             }
-            // const firstNameText = await page.evaluate(firstNameElement => firstNameElement.textContent, firstNameElementHandle);
+            const paginations = await page.$$(".pagination a");
+            console.log("GET OF Paginations : ")
+            console.log('\033[1;37m', '$> ', '\033[1;36m', 'Page suivante', '\033[0m')
+            let rest = await Promise.all([
+                page.waitForNavigation({ timeout: 100000 }),
+                paginations.length == 8 ?
+                    paginations[2].click() :
+                paginations.length == 4 && once == false ?
+                    (once = true,
+                    paginations[0].click()) :(paginations[0].click(),
+                    Promise.resolve('break')) // add a resolved promise here
+            ]);
+            if (rest.includes('break')) {
+                console.log('\033[1;37m', '$> ', '\033[1;36m', 'Fin des profils associés à cette profession', '\033[0m')
+                break;
+            }
+        }   
 
-            const strongText = await page.evaluate(strongElement => strongElement.textContent, strongElementHandle);
-            console.log('profil : ', strongText, ' ', firstNameText.substring(firstNameText.indexOf("</strong>") + "</strong>".length));
-        }
             // currentlist.forEach(element => {
             //     console.log('\033[1;37m', '$> ', '\033[1;32m', 'A trouvé :', element, '✔', '\033[0;37m');
             // });
@@ -78,7 +111,7 @@ const fs = require('fs');
         });
     
     };
-
+    console.log('\e[0m')
     file.end();
 
 })();
